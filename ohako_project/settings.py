24231a3958130e7ab4dfbf27d1610a45b55784fe
Daiki <gotodaiki1110@icloud.com>
2024-06-12 22:02:38 +0900

    debugモードをFalseにしました

--- ohako_project/settings.py
+++ ohako_project/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = ['backendohako-746be3b32e21.herokuapp.com', 'localhost', '127.0.0.1']
 
